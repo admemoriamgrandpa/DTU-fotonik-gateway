9ff3a9b33dc0da876856c108475c7c9f9d98807c
admemoriamgrandpa <nwabuonastanley@gmail.com>
2017-07-05 07:32:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ fi
 
 # Build LoRa gateway app
 if [ ! -d lora_gateway ]; then
-    git clone https://github.com/ttn-zh/lora_gateway.git
+    git clone https://github.com/admemoriamgrandpa/lora_gateway.git
     pushd lora_gateway
 else
     pushd lora_gateway
